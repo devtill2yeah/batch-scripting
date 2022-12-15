8187ec3957ef10db80d6abf94a3dbd366b5365f1
devtill2yeah <devtill2yeah@yahoo.com>
2022-12-15 20:49:22 +0100

    Add files via upload

---
+++ reading registry information.bat
@@ -0,0 +1,16 @@
+
+powershell-scripting 
+
+
+
+
+Get-Item -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion | 
+  Select-Object -ExpandProperty Property#
+
+
+Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion 
+
+
+(New-Object -ComObject WScript.Shell).RegRead("HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\DevicePath")
+
+
---
+++ timesync.bat
@@ -0,0 +1,10 @@
+
+@echo off
+w32tm /query /peers
+sc config w32time start= auto
+w32tm /config /syncfromflags:manual /manualpeerlist:"0.us.pool.ntp.org,1.us.pool.ntp.org,2.us.pool.ntp.org,3.us.pool.ntp.org"
+w32tm /config /reliable:yes
+net stop w32time
+net start w32time
+w32tm /resync /nowait
+
